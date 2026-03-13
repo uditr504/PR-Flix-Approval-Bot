763290170fe56310231df81d01966e6ac481b044
uditr504 <uditr504@gmail.com>
2026-03-13 15:12:57 +0530

    Set default values for configuration variables
    Updated environment variable defaults for API_ID, API_HASH, BOT_TOKEN, SUDO, and MONGO_URI.

--- configs.py
+++ configs.py
@@ -6,14 +6,14 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", ""))
-    API_HASH = getenv("API_HASH", "")
-    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    API_ID = int(getenv("API_ID", "28642558"))
+    API_HASH = getenv("API_HASH", "8ebf6f5403d9494a94f1ee1330027ed8")
+    BOT_TOKEN = getenv("BOT_TOKEN", "8604718668:AAFXqMcI0zeca6ksfxL-msF7MNmNkpBioTY")
     # Your Force Subscribe Channel Id Below 
     CHID = int(getenv("CHID", "")) # Make Bot Admin In This Channel
     # Admin Or Owner Id Below
-    SUDO = list(map(int, getenv("SUDO", "").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    SUDO = list(map(int, getenv("SUDO", "5252260061").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://premashilarana681_db_user:GMucI9xhkmvNUOX2@cluster0.8mlerkv.mongodb.net/?appName=Cluster0")
     
 cfg = Config()
 
